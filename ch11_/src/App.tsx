b273816c7f82566893600abc45df98d6f36d4e27
SEAYOON-JEONG <meriel0486@gmail.com>
2025-05-25 16:20:11 +0900

    a

---
+++ ch11_/src/App.tsx
@@ -0,0 +1,19 @@
+
+import { Button } from 'react-bootstrap'
+import './App.css'
+import Content from './layout/Content'
+import Footer from './layout/Footer'
+
+function App() {
+
+  return (
+    <>
+      
+      
+      <Content />
+      <Footer />
+    </>
+  )
+}
+
+export default App
